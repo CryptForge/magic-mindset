9504340867e0a17bb24dfe0ca8c74e3c3b7dd2a9
Victor <115703994+vschonk@users.noreply.github.com>
2023-03-08 15:30:43 +0100

    Very simple navbar (#10)
    * Very simple navbar, kept a lazy nav in there to reach pages unreachable through nav.
    fixed a typo in the app.jsx, one route path was wrong.
    
    * Added very basic Home page. Lorem text + Button for login. +Home link added

--- website/src/component/Header/Header.jsx
+++ website/src/component/Header/Header.jsx
@@ -1,17 +1,29 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "./Header.css";
 
 const Header = (props) => {
   return (
     <div>
-      <>Hello world! - Header</>
+      <div className="headerflex">
+        <button>Logout/Login WIP</button>
+        <h2>
+          <NavLink to="/studentspage">Studentspage</NavLink>
+        </h2>
+        <h1>
+          <NavLink to="/">Magic Mindset</NavLink>
+        </h1>
+        <h2>
+          <NavLink to="/dashboard">Dashboard</NavLink>
+        </h2>
+        <h2>
+          <NavLink to="/profile">Profile</NavLink>
+        </h2>
+      </div>
       <div>
-        <NavLink to="/">Home</NavLink>
+        <h2>lazy nav, to reach pages not accessible through nav</h2>
         <NavLink to="/login">Login</NavLink>
         <NavLink to="/info">Info</NavLink>
-        <NavLink to="/studentpage">Studentpage</NavLink>
-        <NavLink to="/dashboard">Dashboard</NavLink>
-        <NavLink to="profile">Profile</NavLink>
       </div>
     </div>
   );
