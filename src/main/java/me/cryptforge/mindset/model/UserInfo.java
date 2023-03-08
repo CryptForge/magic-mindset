074852ca770c2af7e837c36dbd8a51e9020b1db4
CryptForge <mail.cryptforge@gmail.com>
2023-03-08 12:24:41 +0100

    Add userinfo entity

---
+++ src/main/java/me/cryptforge/mindset/model/UserInfo.java
@@ -0,0 +1,27 @@
+package me.cryptforge.mindset.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user_info")
+public class UserInfo {
+
+    @Id
+    @OneToOne
+    @JoinColumn(name = "id")
+    private User user;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @Column(name = "city", nullable = false)
+    private String city;
+
+}
