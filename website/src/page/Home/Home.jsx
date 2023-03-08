c7eb010df5454c78220aea6417f756496d6abcf4
Victor <115703994+vschonk@users.noreply.github.com>
2023-03-08 15:33:28 +0100

    Home page branch (#11)
    * Very simple navbar, kept a lazy nav in there to reach pages unreachable through nav.
    fixed a typo in the app.jsx, one route path was wrong.
    
    * Added very basic Home page. Lorem text + Button for login.
    
    ---------
    
    Co-authored-by: CryptForge <70207927+CryptForge@users.noreply.github.com>

--- website/src/page/Home/Home.jsx
+++ website/src/page/Home/Home.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import "./Home.css";
 
 const Home = (props) => {
-  return <>Hello world! - Home</>;
+  return (
+    <div className="homesplitflex">
+      <div className="homecolumnflex">
+        <h1>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Necessitatibus ullam deleniti natus laboriosam in beatae expedita
+          iusto enim doloremque soluta.
+        </h1>
+
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+          perferendis voluptatibus error hic voluptas cum quasi eos quos numquam
+          dolorem vel tempore cumque doloribus consectetur assumenda quae
+          accusamus nobis facere quia minima iure, atque mollitia ut. Fugit,
+          doloribus asperiores quo maiores, consequuntur laboriosam veniam sequi
+          officia libero odit a iure!
+        </h2>
+        <button>LOGIN NOW</button>
+      </div>
+      <div>IMAGE HERE</div>
+    </div>
+  );
 };
 
 export default Home;
