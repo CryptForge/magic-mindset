67dc91fc9e6ef4c42822ca740959b8bd52e121cd
Victor <115703994+vschonk@users.noreply.github.com>
2023-03-08 15:52:09 +0100

    Fixed labels and input fields (#13)

--- website/src/page/Login/Login.jsx
+++ website/src/page/Login/Login.jsx
@@ -5,11 +5,11 @@ const Login = (props) => {
   return (
     <div>
       <form className="loginflexcolumn">
-        <label>email</label>
-        <input></input>
+        <label htmlFor="email">email</label>
+        <input id="email" required></input>
         <p>status</p>
-        <label>password</label>
-        <input></input>
+        <label htmlFor="password">password</label>
+        <input id="password" required></input>
         <p>status</p>
         <input type="submit" value="Login"></input>
       </form>
