bdda7716d8076eec73b6a9726ce0e954b6f4b215
CryptForge <70207927+CryptForge@users.noreply.github.com>
2023-03-08 13:12:20 +0100

    Add skill entity (#5)

--- src/main/java/me/cryptforge/mindset/model/UserInfo.java
+++ src/main/java/me/cryptforge/mindset/model/UserInfo.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @Entity
@@ -23,5 +25,8 @@ public class UserInfo {
 
     @Column(name = "city", nullable = false)
     private String city;
+
+    @OneToMany(mappedBy = "user")
+    private List<Skill> skills;
 
 }
