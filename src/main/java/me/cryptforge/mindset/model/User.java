26330c1921dd53a991d89f3034e1463430ea2f0b
CryptForge <mail.cryptforge@gmail.com>
2023-03-08 11:53:24 +0100

    Add user entity

---
+++ src/main/java/me/cryptforge/mindset/model/User.java
@@ -0,0 +1,34 @@
+package me.cryptforge.mindset.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", length = 16)
+    private Role role;
+
+    /**
+     * Enum containing valid user roles.
+     */
+    public enum Role {
+        TRAINEE, COACH, MANAGER, HR
+    }
+}
