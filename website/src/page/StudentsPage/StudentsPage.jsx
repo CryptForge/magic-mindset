85f72e6adbc6d119fb0c3410a9c8314883df5f7e
vschonk <hundred2netherlands@hotmail.com>
2023-03-09 12:02:28 +0100

    studentinfo page basics

--- website/src/page/StudentsPage/StudentsPage.jsx
+++ website/src/page/StudentsPage/StudentsPage.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import "./StudentsPage.css";
 
 const StudentsPage = (props) => {
-  return <>Hello world! - StudentsPage</>;
+  return (
+    <div classname="flex column">
+      <div className="flex spacearound">
+        <div className="flex column">
+          <h2>Student-Name</h2>
+          <p>Current Skills</p>
+        </div>
+        <button>Dropdown menu</button>
+      </div>
+      <div className="flex spacearound">
+        {/* component here! */}
+        <p>name</p>
+        <p>status</p>
+        <button>See reports: popup</button>
+      </div>
+      <div className="flex spacearound">
+        <button>Back to Dashboard</button>
+        <button>Change User Info</button>
+        <button>Invite to Meeting</button>
+      </div>
+    </div>
+  );
 };
 
 export default StudentsPage;
