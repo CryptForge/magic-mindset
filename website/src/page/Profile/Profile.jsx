71ced723594c34f6ad597df0c2782ab3aea89d31
vschonk <hundred2netherlands@hotmail.com>
2023-03-09 10:56:19 +0100

    very simple profile page

--- website/src/page/Profile/Profile.jsx
+++ website/src/page/Profile/Profile.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import "./Profile.css";
 
 const Profile = (props) => {
-  return <>Hello world! - Profile</>;
+  return (
+    <div className="profileflexcolumn">
+      <form>
+        <div className="flex">
+          <div className="flex column">
+            <label>Profile Image</label>
+            <button>Round button with +</button>
+          </div>
+          <div className="flex column">
+            <label>Name</label>
+            <input></input>
+            <label>E-Mail</label>
+            <input></input>
+            <label>Password</label>
+            <input></input>
+          </div>
+        </div>
+        <div className="flex">
+          <button>Back to Dashboard</button>
+          <input type="submit" value="save"></input>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Profile;
