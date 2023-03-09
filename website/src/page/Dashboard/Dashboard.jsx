f81b043ec4bf93b04fc1ff895fc24ee2f296913d
vschonk <hundred2netherlands@hotmail.com>
2023-03-09 13:34:57 +0100

    studentinfo page basics

--- website/src/page/Dashboard/Dashboard.jsx
+++ website/src/page/Dashboard/Dashboard.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import "./Dashboard.css";
 
 const Dashboard = (props) => {
-  return <>Hello world! - Dashboard</>;
+  return (
+    <div>
+      <div className="grid standardgrid">
+        <div className="box1 gridelement">block 1</div>
+        <div className="box2 gridelement">block 2</div>
+        <div className="box3 gridelement">block 3</div>
+        <div className="box4 gridelement">block 4</div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
