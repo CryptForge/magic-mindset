ba996300cbab68dbac253b4336db6fdf9e2f71a8
Victor <115703994+vschonk@users.noreply.github.com>
2023-03-09 10:38:11 +0100

    very simple info page (#14)

--- website/src/page/Info/Info.jsx
+++ website/src/page/Info/Info.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Info = (props) => {
-  return <>Hello world! - Info</>;
+  return (
+    <div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+        tempore nulla quis mollitia magni illum ea, hic porro dolores
+        repellendus, molestiae doloremque neque itaque illo deserunt, unde alias
+        ducimus. Odio cumque eos nihil repudiandae dolore! Repellat ab eligendi
+        in, facere officia totam ea maxime accusantium voluptatibus? Similique
+        mollitia consequatur natus. Corporis ut voluptas sed impedit quo fugit
+        placeat odit perspiciatis hic, corrupti dolorum accusamus nostrum libero
+        quis iste velit incidunt natus, distinctio expedita error veniam vitae
+        sit eum! In maxime ad, quos enim tenetur nesciunt omnis ipsam quibusdam
+        dignissimos suscipit consequuntur temporibus rerum eos similique saepe
+        pariatur quas, amet quia.
+      </div>
+      <button>Click here!</button>
+    </div>
+  );
 };
 
 export default Info;
