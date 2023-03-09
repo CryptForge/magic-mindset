3b6eff1596d68925f456177c49439c3cd0b122b3
vschonk <hundred2netherlands@hotmail.com>
2023-03-09 14:52:46 +0100

    four components used in dashboard, no popups yet

---
+++ website/src/component/Dashboard/HR/HR.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+const HR = (props) => {
+  return (
+    <div className="grid hrgrid">
+      <div classname="gridelement box1">HRbox1</div>
+      <div classname="gridelement box2">HRbox2</div>
+      <div classname="gridelement box3">HRbox3</div>
+    </div>
+  );
+};
+
+export default HR;
---
+++ website/src/component/Dashboard/User/User.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+const User = (props) => {
+  return (
+    <div className="grid usergrid">
+      <div classname="gridelement box1">Userbox1</div>
+      <div classname="gridelement box2">Userbox2</div>
+      <div classname="gridelement box3">Userbox3</div>
+      <div classname="gridelement box4">Userbox4</div>
+    </div>
+  );
+};
+
+export default User;
