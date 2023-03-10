d8ccf39574692d28746558d9d305da5907e900e7
vschonk <hundred2netherlands@hotmail.com>
2023-03-10 10:24:41 +0100

    all four grid elements in code, no way to switch yet, will be done when authorization is up and running

--- website/src/page/Dashboard/Dashboard.jsx
+++ website/src/page/Dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Dashboard.css";
 import User from "../../component/Dashboard/User/User";
 import HR from "../../component/Dashboard/HR/HR";
@@ -6,14 +6,19 @@ import Coach from "../../component/Dashboard/Coach/Coach";
 import Manager from "../../component/Dashboard/manager/Manager";
 
 const Dashboard = (props) => {
-  return (
-    <div>
-      <User />
-      <HR />
-      <Coach />
-      <Manager />
-    </div>
-  );
+  const [activeComp, setActiveComp] = useState("User");
+
+  switch (activeComp) {
+    case "User":
+      return <User />;
+    case "Coach":
+      return <Coach />;
+    case "Manager":
+      return <Manager />;
+    case "HR":
+      return <HR />;
+  }
+  return "😯";
 };
 
 export default Dashboard;
