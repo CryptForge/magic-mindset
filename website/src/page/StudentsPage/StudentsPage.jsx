e94efcad4db654e16c81057563f51dee3e640b15
vschonk <hundred2netherlands@hotmail.com>
2023-03-10 13:56:49 +0100

    StudentPage now with popups for selecting a student and a popup for reports per skill.

--- website/src/page/StudentsPage/StudentsPage.jsx
+++ website/src/page/StudentsPage/StudentsPage.jsx
@@ -1,22 +1,20 @@
 import React from "react";
+import Skill from "../../component/Skill/Skill";
+import StudentDropdown from "../../component/StudentsPage/StudentDropdown";
+
 import "./StudentsPage.css";
 
 const StudentsPage = (props) => {
   return (
-    <div classname="flex column">
+    <div className="flex column">
       <div className="flex spacearound">
         <div className="flex column">
           <h2>Student-Name</h2>
           <p>Current Skills</p>
         </div>
-        <button>Dropdown menu</button>
+        <StudentDropdown />
       </div>
-      <div className="flex spacearound">
-        {/* component here! */}
-        <p>name</p>
-        <p>status</p>
-        <button>See reports: popup</button>
-      </div>
+      <Skill />
       <div className="flex spacearound">
         <button>Back to Dashboard</button>
         <button>Change User Info</button>
