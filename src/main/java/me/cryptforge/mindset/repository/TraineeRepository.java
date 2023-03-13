242699cd1ca63a57db0f57e53bc05c42ee8d383a
Rebecca de Jong <rebeccadj2003@gmail.com>
2023-03-13 14:37:38 +0100

    Recommendation Endpoints - getAll - getSingleById - getAllByUser - Create

--- src/main/java/me/cryptforge/mindset/repository/TraineeRepository.java
+++ src/main/java/me/cryptforge/mindset/repository/TraineeRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 
 public interface TraineeRepository extends CrudRepository<Trainee, UserInfo> {
     Optional<Trainee> findByUser(UserInfo userInfo);
+
+    Optional<Trainee> findByUser_User_Id(Long id);
 }
