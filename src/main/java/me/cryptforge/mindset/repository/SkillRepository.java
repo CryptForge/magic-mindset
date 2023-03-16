0d9a5ca4e9f0def0d33e668ab28f56974ff5d7d1
CryptForge <70207927+CryptForge@users.noreply.github.com>
2023-03-16 13:32:03 +0100

    Annotate repositories (#50)

--- src/main/java/me/cryptforge/mindset/repository/SkillRepository.java
+++ src/main/java/me/cryptforge/mindset/repository/SkillRepository.java
@@ -2,7 +2,9 @@ package me.cryptforge.mindset.repository;
 
 import me.cryptforge.mindset.model.Skill;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SkillRepository extends CrudRepository<Skill, Long> {
     Iterable<Skill> findAllByTrainee_User_User_Id(Long id);
 }
