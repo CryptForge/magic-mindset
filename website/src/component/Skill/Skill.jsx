bf85211f2fbc5be46e3e445f18082a2f4d23c570
Victor <115703994+vschonk@users.noreply.github.com>
2023-03-16 13:56:00 +0100

    StudentPage updated, also a small change to the dashboard. (#52)

--- website/src/component/Skill/Skill.jsx
+++ website/src/component/Skill/Skill.jsx
@@ -4,10 +4,13 @@ import "./Skill.css";
 
 const Skill = (props) => {
   return (
-    <div className="flex skillflex">
-      <p>name</p>
-      <p>status</p>
-      <Popup trigger={<button>popup button</button>} modal>
+    <div className="flex skill">
+      <p>{props.name}</p>
+      <p>{props.description}</p>
+      <Popup
+        trigger={<button className="skillbutton">See reports</button>}
+        modal
+      >
         <div className="popup">test popup</div>
       </Popup>
     </div>
--- website/src/page/StudentsPage/StudentsPage.jsx
+++ website/src/page/StudentsPage/StudentsPage.jsx
@@ -1,22 +1,61 @@
 import React from "react";
 import Skill from "../../component/Skill/Skill";
 import StudentDropdown from "../../component/StudentsPage/StudentDropdown";
+import { Link } from "react-router-dom";
 
 import "./StudentsPage.css";
 
 const StudentsPage = (props) => {
+  let skillArray = [
+    {
+      type: true,
+      name: "Houthakken 1",
+      description: "Bomen slopen voor hout.",
+      finished: true,
+    },
+    {
+      type: true,
+      name: "Skateboarden 1",
+      description: "Radicaal zijn",
+      finished: false,
+    },
+  ];
+
+  let studentArray = [
+    {
+      name: "Victor",
+    },
+    {
+      name: "Tijs",
+    },
+    { name: "Rebecca" },
+  ];
+
   return (
-    <div className="flex column">
-      <div className="flex spacearound">
+    <div className="flex column element">
+      <div className="flex spacearound columncenter">
         <div className="flex column">
-          <h2>Student-Name</h2>
-          <p>Current Skills</p>
+          <h2>Student - Place McHolder</h2>
+          <h3>Current Skills</h3>
         </div>
-        <StudentDropdown />
+        <StudentDropdown studentList={studentArray} />
       </div>
-      <Skill />
-      <div className="flex spacearound">
-        <button>Back to Dashboard</button>
+      <div className="flex column skillist">
+        {skillArray.map((skill, index) => (
+          <Skill
+            key={index}
+            name={skill.name}
+            finished={skill.finished}
+            description={skill.description}
+            type={skill.type}
+          />
+        ))}
+      </div>
+      <div className="flex spacearound buttonlist">
+        <Link to="/dashboard">
+          <button>Back to Dashboard</button>
+        </Link>
+
         <button>Change User Info</button>
         <button>Invite to Meeting</button>
       </div>
