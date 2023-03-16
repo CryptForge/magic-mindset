77065918d8c6b004f7dbb8da980e2ba97e9f5fc9
Victor <115703994+vschonk@users.noreply.github.com>
2023-03-16 11:32:18 +0100

    Simple dashboard setup using protected classes instead of the switch statement. (#46)

--- website/src/component/Dashboard/manager/Manager.jsx
+++ website/src/component/Dashboard/manager/Manager.jsx
@@ -2,9 +2,15 @@ import React from "react";
 const Manager = (props) => {
   return (
     <div className="grid grid2x2first1x2">
-      <div className="gridelement box1">Managerbox1</div>
-      <div className="gridelement box2">Managerbox2</div>
-      <div className="gridelement box3">Managerbox3</div>
+      <div className="gridelement element box1">
+        <h2>Show all students and also button for single one</h2>
+      </div>
+      <div className="gridelement element box2">
+        <h2>Show invites to feedback</h2>
+      </div>
+      <div className="gridelement element box3">
+        <h2>Plan Meeting</h2>
+      </div>
     </div>
   );
 };
