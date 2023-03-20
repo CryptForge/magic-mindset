f545ba01d3e077b15104e36130dc84e2b7d06260
Ragbecca <55453783+Ragbecca@users.noreply.github.com>
2023-03-20 11:45:38 +0100

    Better explanation of all dashboard items (#58)

--- website/src/component/Dashboard/manager/Manager.jsx
+++ website/src/component/Dashboard/manager/Manager.jsx
@@ -3,13 +3,71 @@ const Manager = (props) => {
   return (
     <div className="grid grid2x2first1x2">
       <div className="gridelement element box1">
-        <h2>Show all students and also button for single one</h2>
+        <div>
+          <h2>Show all students and also button for single one</h2>
+          <ul>
+            <li className="temp-divider">
+              <div>1 - USER 1</div>
+              <div>View (DIFFERENT PAGE)</div>
+            </li>
+            <li className="temp-divider">
+              <div>2 - USER 2</div>
+              <div>View (DIFFERENT PAGE)</div>
+            </li>
+            <li className="temp-divider">
+              <div>3 - USER 3</div>
+              <div>View (DIFFERENT PAGE)</div>
+            </li>
+          </ul>
+        </div>
       </div>
       <div className="gridelement element box2">
-        <h2>Show invites to feedback</h2>
+        <div>
+          <h2>Show invites to feedback</h2>
+          <span>
+            Sorted by date (and after that if accepted, top is not-accepted yet,
+            rest is accepted)
+          </span>
+          <ul>
+            <li className="temp-divider">
+              <div>1 - Meeting 1</div>
+              <div>View (POPUP)</div>
+            </li>
+            <li className="temp-divider">
+              <div>2 - Meeting 2</div>
+              <div>View (POPUP)</div>
+            </li>
+            <li className="temp-divider">
+              <div>3 - Meeting 3</div>
+              <div>View (POPUP)</div>
+            </li>
+          </ul>
+        </div>
       </div>
       <div className="gridelement element box3">
-        <h2>Plan Meeting</h2>
+        <div>
+          <h2>Plan Meeting</h2>
+          <div>
+            <span>Button to plan meeting</span>
+          </div>
+          <div>
+            <span>List with awaiting response</span>
+          </div>
+          <ul>
+            <li className="temp-divider">
+              <div>1 - Meeting 1</div>
+              <div>View (POPUP)</div>
+            </li>
+            <li className="temp-divider">
+              <div>2 - Meeting 2</div>
+              <div>View (POPUP)</div>
+            </li>
+            <li className="temp-divider">
+              <div>3 - Meeting 3</div>
+              <div>View (POPUP)</div>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
