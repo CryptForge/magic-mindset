f6d7577349968f9af91941922cd197dfea04e3fd
vschonk <hundred2netherlands@hotmail.com>
2023-03-20 13:27:36 +0100

    now with new buttons!

---
+++ website/src/component/TraineePage/AddCourseForm.jsx
@@ -0,0 +1,17 @@
+import react from "react";
+
+const AddCourseForm = () => {
+  return (
+    <div className="whiteelement">
+      <form className="flex column">
+        <label htmlFor="">help</label>
+        <input></input>
+        <label htmlFor="">vul dingen hier in</label>
+        <input type="text" />
+        <input type="submit" />
+      </form>
+    </div>
+  );
+};
+
+export default AddCourseForm;
--- website/src/page/TraineePage/SpecificTrainee/SpecificTrainee.jsx
+++ website/src/page/TraineePage/SpecificTrainee/SpecificTrainee.jsx
@@ -1,13 +1,67 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
+import Popup from "reactjs-popup";
+import "./SpecificTrainee.css";
+import AddCourseForm from "../../../component/TraineePage/AddCourseForm";
 
 const SpecificTrainee = () => {
+  const [activeSkill, activateSkill] = useState();
+
+  const courseArray = [
+    {
+      name: "course1",
+    },
+    {
+      name: "course2",
+    },
+    {
+      name: "course3",
+    },
+  ];
+
+  const skillArray = [
+    {
+      name: "Woodcutting",
+    },
+    {
+      name: "Planning",
+    },
+    {
+      name: "Astral Projection",
+    },
+  ];
   const { traineeId } = useParams();
 
   return (
     <div>
       <h1>{traineeId}</h1>
+      <h2>Trainee name and skilllist</h2>
+      <div className="element flex column list">
+        {skillArray.map((skill, index) => (
+          <div>
+            <div key={index} className="listelement flex space-around">
+              <h3>{skill.name}</h3>
+              <div className="flex space-between buttonspacing">
+                <button onClick={() => activateSkill(index)}>
+                  Show Courses
+                </button>
+                <Popup trigger={<button>Add Course</button>} modal>
+                  <AddCourseForm />
+                </Popup>
+                <button>Show Reports</button>
+              </div>
+            </div>
+            <div id={index} className={activeSkill == index ? "" : "hidden"}>
+              {courseArray.map((course, index) => (
+                <div key={index}>
+                  <p>{course.name}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
       <Link to="/traineepage">
         <button>Back to Traineepage!</button>
       </Link>
