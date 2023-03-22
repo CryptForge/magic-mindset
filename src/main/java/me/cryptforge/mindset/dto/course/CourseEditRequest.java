b59d7f7fcf88ee32577c84cbbc2ac1fb1f52efb8
CryptForge <mail.cryptforge@gmail.com>
2023-03-22 11:35:56 +0100

    Add name to course

--- src/main/java/me/cryptforge/mindset/dto/course/CourseEditRequest.java
+++ src/main/java/me/cryptforge/mindset/dto/course/CourseEditRequest.java
@@ -2,7 +2,9 @@ package me.cryptforge.mindset.dto.course;
 
 public record CourseEditRequest(
         Long id,
+        String name,
         Long skillId,
         float progress,
-        String certification) {
+        String certification
+) {
 }
