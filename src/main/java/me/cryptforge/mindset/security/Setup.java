23b874f83e9d7d89262bedd2cfc873473c3b457e
vschonk <hundred2netherlands@hotmail.com>
2023-03-24 10:42:41 +0100

    Testtrainees, coaches, managers and HRs! Oh my!

--- src/main/java/me/cryptforge/mindset/security/Setup.java
+++ src/main/java/me/cryptforge/mindset/security/Setup.java
@@ -1,5 +1,7 @@
 package me.cryptforge.mindset.security;
 
+import me.cryptforge.mindset.dto.user.EditCoachInTraineeRequest;
+import me.cryptforge.mindset.dto.user.EditManagerInTraineeRequest;
 import me.cryptforge.mindset.dto.user.UserRequest;
 import me.cryptforge.mindset.model.user.User;
 import me.cryptforge.mindset.service.AuthService;
@@ -35,6 +37,36 @@ public class Setup {
             ));
             authService.verify(username);
         }
+        long testTrainee1 = createTestUser("TestTraineeMan 1", User.Role.TRAINEE);
+        long testTrainee2 = createTestUser("TestTraineeMan 2", User.Role.TRAINEE);
+        long testTrainee3 = createTestUser("TestTraineeMan 3", User.Role.TRAINEE);
+        long testCoach1 = createTestUser("TestCoach 1", User.Role.COACH);
+        long testCoach2 = createTestUser("TestCoach 2", User.Role.COACH);
+        long testManager = createTestUser("TestManagerMan", User.Role.MANAGER);
+        long testHR = createTestUser("TestHRMan", User.Role.HR);
+
+        linkCoach(testTrainee1, testCoach2);
+        linkCoach(testTrainee2, testCoach1);
+        linkCoach(testTrainee3, testCoach1);
+        linkManager(testTrainee1, testManager);
+        linkManager(testTrainee2, testManager);
+        linkManager(testTrainee3, testManager);
+
+
+    }
+
+    private long createTestUser(String name, User.Role role) {
+        var user = userService.createUser(new UserRequest(name, name, role, name, name + " Street", name + " City"));
+        authService.verify(name);
+        return user.getId();
+    }
+
+    private void linkCoach(long traineeId, long coachId) {
+        userService.changeCoachTrainee(new EditCoachInTraineeRequest(traineeId, coachId));
+    }
+
+    private void linkManager(long traineeId, long managerId) {
+        userService.changeManagerTrainee(new EditManagerInTraineeRequest(traineeId, managerId));
     }
 
 }
