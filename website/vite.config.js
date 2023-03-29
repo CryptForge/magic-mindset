e32e3134562580f75775e4af7e265ee136a9164e
CryptForge <mail.cryptforge@gmail.com>
2023-03-29 10:57:21 +0200

    Fix prod build

--- website/vite.config.js
+++ website/vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "../src/main/resources/static/",
     emptyOutDir: true
   }
 })
