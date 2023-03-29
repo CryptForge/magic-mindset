6d9395230fd8134990926399fd4cd5fa3833512e
vschonk <hundred2netherlands@hotmail.com>
2023-03-29 13:03:38 +0200

    Changes to invitation forms.

--- website/src/component/Dashboard/Common/AddInvitiationForm.jsx
+++ website/src/component/Dashboard/Common/AddInvitiationForm.jsx
@@ -8,21 +8,21 @@ const AddInvitationForm = (props) => {
     <form
       className="flex flex-column white-element"
       onSubmit={(event) =>
-        authPostForm(
-          event,
-          `${API_BASE}/invitation/create`,
-          auth.getUser().token
-        )
+        authPostForm(event, `${API_BASE}/evaluation`, auth.getUser().token)
       }
     >
       <label htmlFor="trainee">Trainees</label>
       <select id="trainee" name="trainee">
         {props.traineeList.map((trainee, index) => (
-          <option value={trainee.id}>{trainee.username}</option>
+          <option key={index} value={trainee.id}>
+            {trainee.username}
+          </option>
         ))}
       </select>
       <label htmlFor="date">Date</label>
-      <input type="date" id="date"></input>
+      <input type="datetime-local" id="date"></input>
+      <label htmlFor="location">Location</label>
+      <input type="text" id="location"></input>
       <input type="submit" value="invite!" />
     </form>
   );
